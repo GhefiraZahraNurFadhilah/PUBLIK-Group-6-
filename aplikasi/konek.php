9729151080cdbe3439c6a1131614325e7c64f49f
GhefiraZahraNurFadhilah <gehfirazahra22055@gmail.com>
2023-12-28 00:37:51 -0800

    Ghefira Zahra Nur Fadhilah_E1E122055

--- aplikasi/konek.php
+++ aplikasi/konek.php
@@ -1,18 +1,17 @@
 <?php
-// E1E122004 ANNISA NURFADILAH
+
 class Database
 {
-    // Properti dengan visibility diawali sebelum properti untuk menambahkan kejelasan dalam pembacaan kode.
     private $host = "localhost", $username = "root", $password = "", $db = "db_publik", $conn;
+     // ini adalah salah satu contoh yang menerapkan konsep encapsulation yang dimana host,username,password, dan db dan conn nya itu bersifat privat yang artinya kita membatasi akses langsung dari luar kelas jadi hanya method yang ada didalam kelas database saja yang dapat mengakses atribut ini.
 
-    // Konstruktor, merupakan magic method pada PHP, digunakan untuk inisialisasi objek saat objek dibuat.
     public function __construct()
     {
-        // Menghubungkan ke database menggunakan properti yang telah ditentukan sebelumnya.
+       
         $this->conn = mysqli_connect($this->host, $this->username, $this->password, $this->db);
     }
 
-    // Metode getKonek digunakan untuk mengembalikan nilai dari properti $conn.
+    
     public function getKonek()
     {
         return $this->conn;
